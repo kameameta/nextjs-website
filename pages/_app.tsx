121aa981b58736bbe2b452c2e8f8cfbfa951353b
mezzo <mezzo@neonnoon.ch>
2021-12-04 08:03:48 +0100

    eslint > prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 
 import { AppProps } from 'next/app';
 
-function App({ Component, pageProps }: AppProps) : React.ReactElement {
+function App({ Component, pageProps }: AppProps): React.ReactElement {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import Head from 'next/head';
 import styles from '../styles/index.module.css';
 
-const Index = () : React.ReactElement => (
+const Index = (): React.ReactElement => (
   <div className={styles.container}>
-
     <Head>
       <title>Welcome!</title>
       <link rel="icon" href="/favicon.png" />
